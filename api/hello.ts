71d9d48c09d14ceb836d52461fa44edda3e9bf97
Valerio Luis <valeriouniversidad@gmail.com>
2025-03-29 15:32:20 +0000

    trying fixing path

--- api/hello.ts
+++ api/hello.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { id = '001' } = req.query
   
-  const data = fs.readFileSync('../data/boya'+id+'.json', 'utf-8');
+  const data = fs.readFileSync('./data/boya'+id+'.json', 'utf-8');
   const json = JSON.parse(data);
   
   return res.json(json)
