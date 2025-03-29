9f5b4a1fde02bc65db133790ba162954783f2503
Valerio Luis <valeriouniversidad@gmail.com>
2025-03-29 15:01:09 +0000

    json test

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,14 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'Tecnologica 25' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+  const { id = '001' } = req.query
+  
+  var json = {
+    id: id,
+  }
+    
+  
+  return res.json(json)
+
+
 }
