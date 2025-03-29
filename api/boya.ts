e85f5dfbbff8a97cb2b4f33b3a91159a106fe808
Valerio Luis <valeriouniversidad@gmail.com>
2025-03-29 16:01:02 +0000

    both end point first try

---
+++ api/boya.ts
@@ -0,0 +1,17 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import * as fs from 'fs';
+import path from 'path';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { id = '001' } = req.query
+  
+  const filePath = path.join(process.cwd(), 'public', 'data', `boya${id}.json`);
+
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const json = JSON.parse(data);
+    return res.status(200).json(json);
+  } catch (error) {
+    return res.status(404).json({ error: `Boya con id "${id}" no encontrada` });
+  }
+}
--- api/locations.ts
+++ api/locations.ts
@@ -3,21 +3,15 @@ import * as fs from 'fs';
 import path from 'path';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { id = '001' } = req.query
-  
-  const filePath = path.join(process.cwd(), 'public', 'data', `boya${id}.json`);
+  const { user = 'anonymous' } = req.query
+
+  const filePath = path.join(process.cwd(), 'public', 'general', 'locations.json');
 
   try {
     const data = fs.readFileSync(filePath, 'utf-8');
     const json = JSON.parse(data);
     return res.status(200).json(json);
   } catch (error) {
-    return res.status(404).json({ error: 'File not found' });
+    return res.status(500).json({ error: 'location data not found' });
   }
-
-
-  const data = fs.readFileSync('./data/boya'+id+'.json', 'utf-8');
-  const json = JSON.parse(data);
-  
-  return res.json(json)
 }
