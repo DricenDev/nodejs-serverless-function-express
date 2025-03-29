be5691671e055b677985e39b3e5a2f682ea628fe
Valerio Luis <valeriouniversidad@gmail.com>
2025-03-29 15:29:30 +0000

    first json on data

--- api/hello.ts
+++ api/hello.ts
@@ -1,14 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import * as fs from 'fs';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { id = '001' } = req.query
   
-  var json = {
-    id: id,
-  }
-    
+  const data = fs.readFileSync('../data/boya'+id+'.json', 'utf-8');
+  const json = JSON.parse(data);
   
   return res.json(json)
-
-
 }
